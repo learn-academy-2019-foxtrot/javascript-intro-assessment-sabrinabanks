3d00120c12aef52b47742cb2548bd1d42bc3ef42
Ubuntu <ubuntu@ip-172-31-28-132.us-east-2.compute.internal>
2019-11-25 17:42:04 +0000

    submit1st

--- js-intro.js
+++ js-intro.js
@@ -7,15 +7,15 @@ var mantra = "Be the dev"
 
 // 1a. Write the code that determines if there is a 'B' in mantra.
 
-
+console.log(mantra.includes("B"));
 
 // 1b. Write the code that determines if there is an 'x' in mantra.
 
-
+console.log(mantra.includes("x"));
 
 // 1c. Write the code that determines if there is a 'v' in mantra.
 
-
+console.log(mantra.includes("v"));
 
 // 1d. STRETCH: Write a function that can take two arguments - a string and a letter and returns a sentence telling the user if the letter exists within the string.
 
@@ -28,11 +28,27 @@ var myCat = "Garfield"
 
 // 2a. Create a function that returns the longer of the two variables. Expected output: "Garfield"
 
-
-
+var lengthcheck = (pet1,pet2) => {
+    if (pet1.length > pet2.length) {
+         return pet1
+        
+    } else if (pet1.length < pet2.length) {
+         return pet2
+        
+    } else {
+        return "they are equal" 
+    }
+}
+console.log(lengthcheck(myDog,myCat));
 
 // 2b. Create a function that takes in two arguments and returns an array containing the two variables. Expected output: ["Snoopy", "Garfield"]
 
+var hooArray = (pet1,pet2) => {
+    var newArray = [pet1,pet2] 
+    return newArray
+}
+
+console.log(hooArray(myDog,myCat));
 
 
 
@@ -48,9 +64,26 @@ var myMessage = "Learning is fun!"
 // 3a. Write the code that logs each letter of the message using a for loop.
 //hint: "logs" in this case means "prints to the terminal console."  This is typically done with a console.log(x), where x is the string, function, etc that you wish to see in the terminal.
 
+// var arr =  ["l","e","a","r","n","i","n","g"," ","i","s"," ","f","u","n"]
+
+const logletter = (string) ={
+for(let i = 0; i < arr.length; i++){
+    console.log(arr[i]);
+    }
+}
+
 
 // 3b. Write the code that logs each letter of the message using map.
 
+// var arr = ["l","e","a","r","n","i","n","g"," ","i","s"," ","f","u","n"]
+
+
+const logLetter = (array) => {
+    let newarray = array.split("")
+    return newarray.map((v,i), { 
+        console.log (array[i])
+    })
+}
 
 
 // 3c. STRETCH: Write the code that logs each letter of the message using a while loop.
@@ -68,9 +101,15 @@ var testString = "thisisateststring"
 
 // 4a. Write a function that takes the variable as an argument and returns the string without vowels. Expected output: "thsstststrng"
 
-
-
-
+var byeVowel = (string) => {
+    var empty = ""
+    for(let i=0; i<string.length; i++){
+      if(string[i] !== "a","e","i","o","u") {
+          empty += string[i]
+      } 
+        console.log(testString[i])
+    }
+}
 
 // 4b. STRETCH: Update your function to return an error message if the variable is not a string. Copy and paste your function to add these changes.
 
@@ -85,12 +124,29 @@ var toonimals = [ {name: "Itchy", animal: "mouse"}, {name: "Stimpy", animal: "ca
 
 // 5a. Write a function that takes in the toonimals array as an argument and returns an array with only the toon objects that are cats. Expected output: [ { name: "Stimpy", animal: "cat" }, { name: "Scratchy", animal: "cat" }, { name: "Felix", animal: "cat" } ]
 
+const onlyCat = (array) => {
+    let newArr = array.filter((v,i) =>
+    if (v.animal === "cat"){
+    return v
+}
+console.log(newArr)
+}
 
-
+onlyCats(toonimals)
 
 //5b. Write a function that returns only the names of all the non-cats. Expected output: "Itchy" "Daffy" "Ren"
 
 
+const otherName = (array) => {
+    let newArr = array.filter((v,i)) => {
+        if(v.animal !== "cats"){
+    return v.name
+    }
+    
+}
+console.log(newArr))
+}
+otherName(toonimals)
 
 
 //5c. STRETCH: Create a function that will print a sentence for every toon stating their name and their animal type. Use string interpolation. Expected output: "Itchy is a mouse" "Stimpy is a cat" "Daffy is a duck" "Scratchy is a cat" "Ren is a dog" "Felix is a cat"
--- js-intro.md
+++ js-intro.md
@@ -5,49 +5,60 @@ Answer the following questions. First, without external resources. Challenge you
 
 1. What is a function? Why would you use one?
 
-  Your answer:
+  Your answer: A function is something that does something and needs to be called to action.
 
-  Researched answer:
+  Researched answer:Functions are one of the fundamental building blocks in JavaScript. 
+  A function is a JavaScript procedure—a set of statements that performs a task or calculates a value. 
+  To use a function, you must define it somewhere in the scope from which you wish to call it.
 
 
 
 2. List 5 examples of JavaScript array methods.
 
-  Your answer:
+  Your answer:? .pop .push .shift .unshift. indexof 
 
   Researched answer:
-
-
+  var first = fruits.shift();
+  var newLength = fruits.unshift('Strawberry');
+ 
 
 3. Explain the difference between console.log() and return.
 
-  Your answer:
+  Your answer: return calls something to action and console.log() show the outcome onto the terminal, 
 
-  Researched answer:
+  Researched answer:return is a statement that allows a function to output a value back to where it was called. 
+  console.log is a function that lets us inspect values for debugging purposes.
 
 
 
 4. In regards to functions, what is an argument? What is the difference between an argument and a variable?
 
-  Your answer:
+  Your answer: a variable can change and is also declaring the name of an item. an argument is code that is stated as such.
 
-  Researched answer:
-
+  Researched answer:Arguments are values passed to the function when it is invoked. Variable means anything that can vary. 
+  JavaScript includes variables which hold the data value and it can be changed anytime.
+  
 
 
 5. Give a brief description of proper pair programming techniques. What are the roles of each person?
 
-  Your answer:
+  Your answer: there is a driver and navigator , driver types and navigator says what they should write.
 
-  Researched answer:
+  Researched answer:Pair programming a technique in which two programmers work together at one workstation. 
+  One, the driver, writes code while the other, the observer or navigator,
+  one reviews each line of code as it is typed in. The two programmers switch roles frequently.
+  While reviewing, the navigator also considers the "strategic" direction of the work, 
+  coming up with ideas for improvements and likely future problems to address.
+  the driver to focus all of their attention on the "tactical" aspects of completing the current task,
+  using the observer as a safety net and guide.
 
 
 
 6. Give a brief description of JavaScript as if you were explaining it to someone who doesn't know anything about programming.
 
-  Your answer:
+  Your answer: javascript is the building blocks of coding and is more advanced then html.it allowes  people to complete actions more easily.
 
 
 7. What is one thing we did in class this week you found helpful?  
 
-  Your answer:
+  Your answer: doing both of these assignments alone.
